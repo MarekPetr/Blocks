c402743caa92b80c76bc34b2eb6a40259e2a1d31
Petr Marek <petr.marek18@gmail.com>
2018-04-26 23:04:01 +0200

    OUT blok hodnoty
    Vypis a aktualizace hodnot v OUT bloku na kliknuti stredu. Debilni, ale funkcni.

--- src/project/GUI/DraggableNodeOUT.java
+++ src/project/GUI/DraggableNodeOUT.java
@@ -2,6 +2,10 @@ package project.GUI;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+import java.lang.reflect.Field;
+import java.util.Map;
 
 /**
  * Created by petr on 4/24/18.
@@ -13,6 +17,7 @@ public class DraggableNodeOUT extends DraggableNode {
     @FXML private Label value2;
     @FXML private Label key3;
     @FXML private Label value3;
+    @FXML private VBox input_table;
 
 
     public DraggableNodeOUT(RootLayout layout) {
@@ -27,9 +32,37 @@ public class DraggableNodeOUT extends DraggableNode {
 
     @Override
     public void buildInputHandlers() {
+        Class currentClass = getClass();
+        Field[] fields = currentClass.getFields();
+        for (Field f : fields) {
+            System.out.println(f.getName());
+        }
+        input_table.setVisible(false);
         body_handle.setOnMouseClicked(event -> {
             //print values here
-            System.out.println(layout.blocks.get(getId()).item.outValue);
+            Map<String, Double> map = layout.blocks.get(getId()).item.outValue;
+            int i = 1;
+            for (Map.Entry<String, Double> entry : map.entrySet())
+            {
+                System.out.println(entry.getKey() + "/" + entry.getValue());
+                String value = String.valueOf(entry.getValue());
+                if (i == 1) {
+                    key1.setText(entry.getKey());
+                    value1.setText(value);
+                } else if (i == 2) {
+                    key2.setText(entry.getKey());
+                    value2.setText(value);
+                } else if (i == 3) {
+                    key3.setText(entry.getKey());
+                    value3.setText(value);
+                }
+                i++;
+            }
+            if (input_table.isVisible()) {
+                input_table.setVisible(false);
+            } else {
+                input_table.setVisible(true);
+            }
         });
 
     }
